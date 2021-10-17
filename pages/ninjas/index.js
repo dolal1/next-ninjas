1346566a0c7e20b0c040f8142335f82095b008be
David OH <holifeilddavid@gmail.com>
2021-10-17 13:46:49 +0300

    feat: Dynamic Rendering of Solo Ninja Pages

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,10 +1,36 @@
 import React from "react";
+import Head from "next/head";
+import Link from "next/link";
 
-function index() {
+import styles from "../../styles/Ninjas.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+function index({ ninjas }) {
   return (
-    <div>
-      <h1>All Ninjas</h1>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | All Ninjas</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>All Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
 
